4c2a6645d60b3ec5aeafcda6310778853607e6e6
农元辉 <116888338@qq.com>
2023-04-27 09:01:31 +0800

    代码提交

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,19 @@
 // .vuepress/config.js
 module.exports = {
-    themeConfig: {
-      sidebar: [
-        // '/', 
-        '/componentDocs/hello', 
-        '/componentDocs/card',                
-      ]
-    }
+  title: '/Mooc-UI',
+  base: '/moocUI',
+  themeConfig: {
+    nav: [{
+      text: 'Home', link: '/'
+    }, {
+      text: 'Github', link: 'https://github.com/nongyuanhui/moocui'
+    }, {
+      text: 'Npm', link: 'https://www.npmjs.com/package/nongyuanhui-ui'
+    }],
+    sidebar: [
+      // '/', 
+      '/componentDocs/hello',
+      '/componentDocs/card',
+    ]
   }
+}
